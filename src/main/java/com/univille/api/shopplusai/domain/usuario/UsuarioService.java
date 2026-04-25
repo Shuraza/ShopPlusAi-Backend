3527e933547db8c5a99ba0e7124806a870a6fdcd
Caio <caiodesouza.cds@gmail.com>
2026-04-25 11:09:01 -0300

    feat: cria metodo de cadastrar no usuario service

---
+++ src/main/java/com/univille/api/shopplusai/domain/usuario/UsuarioService.java
@@ -0,0 +1,19 @@
+package com.univille.api.shopplusai.domain.usuario;
+
+import com.univille.api.shopplusai.domain.usuario.dto.CreateUsuarioRequest;
+import com.univille.api.shopplusai.domain.usuario.dto.UsuarioResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioResponse save(CreateUsuarioRequest dados){
+        var usuario = new Usuario(dados);
+        repository.save(usuario);
+        return new UsuarioResponse(usuario);
+    }
+}
