c32de64ab838f8468d72a27d3b8b7250f0448903
Caio <caiodesouza.cds@gmail.com>
2026-04-25 13:04:14 -0300

    feat: cria tratamento de excecao para campos invalidos das requisicoes lancadas pelo bean validation

--- src/main/java/com/univille/api/shopplusai/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/com/univille/api/shopplusai/infra/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.univille.api.shopplusai.infra.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +15,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e){
         var responseError = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage(), null);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        var errors = e.getFieldErrors();
+        var responseError = new ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+    "Erro de validação dos campos",
+            errors.stream().map(ErrorValidationResponse::new).collect(Collectors.toList())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseError);
     }
 }
