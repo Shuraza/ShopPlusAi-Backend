00be79092c1078e73424a879936c17ec6c880075
Caio <caiodesouza.cds@gmail.com>
2026-04-25 14:32:02 -0300

    feat: cria entidade de categorias

---
+++ src/main/java/com/univille/api/shopplusai/domain/categoria/Categoria.java
@@ -0,0 +1,22 @@
+package com.univille.api.shopplusai.domain.categoria;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categorias")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = false)
+    private String nome;
+}
