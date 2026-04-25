6f46e2f0d2685e4cf588b739f8662cc16204aca4
Caio <caiodesouza.cds@gmail.com>
2026-04-25 11:16:09 -0300

    feat: cria exception de not found

---
+++ src/main/java/com/univille/api/shopplusai/infra/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.univille.api.shopplusai.infra.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
