dc6b9776613084f86a65b05e7aff22b1d8eb78ca
Caio <caiodesouza.cds@gmail.com>
2026-05-07 17:26:00 -0300

    feat: ajusta dto de resposta do produto

--- src/main/java/com/univille/api/shopplusai/domain/produto/dto/ProdutoResponse.java
+++ src/main/java/com/univille/api/shopplusai/domain/produto/dto/ProdutoResponse.java
@@ -1,11 +1,12 @@
 package com.univille.api.shopplusai.domain.produto.dto;
 
+import com.univille.api.shopplusai.domain.categoria.Categoria;
 import com.univille.api.shopplusai.domain.produto.Produto;
 import org.jspecify.annotations.Nullable;
 
 
-public record ProdutoResponse(Long id, String nome, Long idCategoria, double preco) {
-    public ProdutoResponse(Produto produto){this(produto.getId(), produto.getNome(), produto.getIdCategoria(), produto.getPreco());}
+public record ProdutoResponse(Long id, String nome, Categoria categoria, double preco) {
+    public ProdutoResponse(Produto produto){this(produto.getId(), produto.getNome(), produto.getCategoria(), produto.getPreco());}
 
 }
 
